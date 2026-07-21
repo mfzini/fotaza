6639eec7f5996c48463f921cd3acff44daebbe4f
Cisco <mfzini.dev@gmail.com>
2026-07-21 02:22:56 -0300

    mas retoques

--- src/controllers/mailController.ts
+++ src/controllers/mailController.ts
@@ -4,9 +4,9 @@ import { Mail } from '../models/Mail.js';
 
 export async function mailView(req: Request, res: Response, next: NextFunction) {
     const userId = (req.user as User).id;
-    let inbox = await Mail.findAll({ where: { toId: userId }, include: ['from'] });
+    let inbox = await Mail.findAll({ where: { toId: userId }, include: ['from'], order: [['createdAt', 'ASC']] });
 
-    let outbox = await Mail.findAll({ where: { fromId: userId }, include: ['to'] });
+    let outbox = await Mail.findAll({ where: { fromId: userId }, include: ['to'], order: [['createdAt', 'ASC']] });
     res.render('mail', { inbox, outbox });
 }
 
